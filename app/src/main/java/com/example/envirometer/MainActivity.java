47602fceafad2b930a33a60cd98fe875a31044d7
Rob Cheung <22031605+RobCheung888@users.noreply.github.com>
2021-08-06 21:08:25 -0400

    first commit

---
+++ app/src/main/java/com/example/envirometer/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.envirometer;
+
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item)
+        {
+            Fragment fragment = null;
+
+            switch (item.getItemId())
+            {
+                case R.id.home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.target:
+                    fragment = new TargetFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
+        }
+    };
+}
